e5a0edad7d857ff24e469e9ea9c3b139151b4f51
Tomas Roun <tomas.roun@seznam.cz>
2021-04-25 19:12:00 +0200

    update memory

--- memory.py
+++ memory.py
@@ -9,19 +9,21 @@ import matplotlib.pyplot as plt
 def get_particle_memory(n_landmarks):
     return 6 + 7*n_landmarks
 
-def get_memory(n_particles, n_landmarks):
+def get_memory(n_particles, n_landmarks, known=False):
     memory = 0
     memory += n_particles * get_particle_memory(n_landmarks)
     memory += 2 * n_landmarks
     memory += 6 *n_particles
-    memory += 2 * min(n_particles, 512) * n_landmarks
+
+    if not known:
+        memory += 2 * n_particles * n_landmarks
 
     memory *= 8
 
     return memory/(1024**2)
 
 
-EXPORT = True
+EXPORT = False
 
 if EXPORT:
     matplotlib.use("pgf")
@@ -43,11 +45,15 @@ fig.subplots_adjust(left=0.13, right=0.99, bottom=0.18, top=0.99)
 # memory = [get_memory(np, n_landmarks) for np in particles]
 # print(memory)
 
-n_particles = 1000
+n_particles = 128
 landmarks = [1, 10, 100, 1000, 10000]
-memory = [get_memory(n_particles, nl) for nl in landmarks]
+memory_unknown = [get_memory(n_particles, nl) for nl in landmarks]
+memory_known = [get_memory(n_particles, nl, known=True) for nl in landmarks]
+
 # print(memory)
-ax.plot(landmarks, memory[:])
+ax.plot(landmarks, memory_unknown[:])
+ax.plot(landmarks, memory_known[:])
+
 
 ax.grid()
 ax.set_ylabel("MB")
